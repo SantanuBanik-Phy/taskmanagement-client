0d97e1649f72f5246961033228a074cdf7cc656c
Santanu Banik <Santanubanik2k@gmail.com>
2025-02-20 22:22:45 +0600

    draghandle component added

---
+++ src/components/DragHandle.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const DragHandle = () => {
+  return (
+    <div className="cursor-grab p-2 flex justify-center items-center bg-gray-200 rounded-md">
+      <span className="text-gray-500">☰</span> 
+    </div>
+  );
+};
+
+export default DragHandle;
